3c32add9dd3358e986ef71bc304399986f503b0e
Rahima Mokter <rahimamokter@gmail.com>
2020-10-11 01:45:52 -0400

    Lab 8 Q 2 Completed

--- src/app/cardlogo.directive.ts
+++ src/app/cardlogo.directive.ts
@@ -5,45 +5,49 @@ import { Directive, Input, OnChanges, HostBinding } from '@angular/core';
   
 })
 export class CardlogoDirective implements OnChanges {
-  cardType: string = "Discover";
+  //cardType: string = "";
  
+  constructor() { }
+
+  @Input() cardNumber: string;
+  @Input() cardType: string;
+  
+  @HostBinding('src') imageSource;
+  
 ngOnChanges() {
   
-  this.imageSource = './assets/default.JPG';
+  this.imageSource = './assets/default.jpg';
 
+ 
   if(this.cardNumber.startsWith("34")) {
-    this.imageSource = './assets/amex.JPG';
+    this.imageSource = './assets/amex.jpg';
     this.cardType = "Amex";
   }
   
   else if(this.cardNumber.startsWith("30")) {
-    this.imageSource = './assets/diners.JPG';
+    this.imageSource = './assets/diners.jpg';
     this.cardType = "Diners";
   }
 
   else if(this.cardNumber.startsWith("60")) {
-      this.imageSource = './assets/discover.JPG';
+      this.imageSource = './assets/discover.jpg';
       this.cardType = "DISCOVER";
     }
     
     else if(this.cardNumber.startsWith("5")) {
-      this.imageSource = './assets/mastercard.JPG';
+      this.imageSource = './assets/mastercard.jpg';
       this.cardType = "MASTERCARD";
     }
     
     else if(this.cardNumber.startsWith("4")) {
-      this.imageSource = './assets/visa.JPG';
+      this.imageSource = './assets/visa.jpg';
       this.cardType = "VISA";
     }
+    else {
+      this.imageSource = './assets/default.jpg';
+      this.cardType = "Default";
+    }
     
-   
   
 }
-
-constructor() { }
-
-  @Input() cardNumber: string;
-  //@Input() cardType: string;
-  
-  @HostBinding('src') imageSource;
 }
